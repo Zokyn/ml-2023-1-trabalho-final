314a668325a8d1061694934be25ff790898f3e24
Zokyn <gustavo.casaes@mail.uft.edu.br>
2023-06-27 17:14:28 -0300

    FEAT primeira versão final do modelo

--- main.py
+++ main.py
@@ -53,20 +53,23 @@ def print_cloudword(df):
     plt.imshow(wordcloud)
     plt.title("Todas as palavras no review")
     plt.show()
+
+## 1.LENDO O ARQUIVO DE ENTRADA
 dataset = pd.read_csv("New_Delhi_reviews.csv")
 
 data = dataset.copy()
 
-## REMOVENDO VALORES VAZIOS
+## 2.REMOVENDO VALORES VAZIOS
 data.dropna(inplace=True)
 data.reset_index(drop=True, inplace=True)
 
-## LIMPANDO OS DOCUMENTOS
+## 3.LIMPANDO OS DOCUMENTOS
 # foi retirado prefixos de url de possiveis links
 # no comentario de review, além disso:
 #   1. colocado as palavras em minusculas
 #   2. removindo qualquer stopword no texto
-#   3. transoformado em um novo dataframe
+#   3. feito o processo de stemming nos tokens
+#   4. e transoformado em um novo dataframe
 corpus = []
 for i in range(len(data)):
     review = data['review_full'][i]
@@ -82,9 +85,60 @@ for i in range(len(data)):
     review = " ".join(review) 
 
     corpus.append(review)
+    
+# *preciso revisar oq ue acontecer aqui* 
+corpus = pd.DataFrame(corpus, columns=["review_full"])
+corpus["rating_review"] = data["rating_review"]
+# corpus_sorted = corpus.groupby(["review_full"]).nunique().sort_values(by='rating_review', ascending=False)
+# corpus_sorted = corpus_sorted[corpus_sorted["rating_review"] > 1]["rating_review"]
+# k = corpus_sorted.index
 
-corpus = pd.DataFrame(corpus, columns=["review"])
-corpus["rating"] = data["rating_review"]
-print(corpus)
+# for h in range(len(k)):
+#     corpus.loc[corpus["review_full"] == k[h], "rating_review"] = round(average_of_rating(corpus.loc[corpus["review_full"] == k[h], "rating_review"]))
+# # *ate aqui mais ou menos* 
+
+data["stem_review"] = corpus["review_full"]
+
+## 4.SEPARANDO OS DADOS
+# é preciso separar os conjuntos 
+# de treino e teste dos dados e
+# utilizar um vectorizer para a 
+# frequencia de palavras no review
+
+x = data["stem_review"].values
+y = data["rating_review"].values
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.7, random_state=1)
+
+vectorizer = CountVectorizer(max_features=1500)
+x_train = vectorizer.fit_transform(x_train).toarray()
+x_test = vectorizer.transform(x_test).toarray()
+
+## 5.CONSTRUINDO UM CLASSIFICADOR
+# usando Naive Bayes
+method = GaussianNB()
+method.fit(x_train, y_train)
+y_pred = method.predict(x_test)
+
+# usando RandomForest
+method = RandomForestClassifier(n_estimators=1000)
+method.fit(x_train, y_train)
+y_pred2 = method.predict(x_test) 
+
+# Usando Regressão
+method = LogisticRegression(max_iter=1000)
+method.fit(x_train, y_train)
+y_pred3 = method.predict(x_test)
+
+## 6.ACESSANDO OS RESULTADOS
+accuracy = metrics.accuracy_score(y_test, y_pred)
+accuracy2 = metrics.accuracy_score(y_test, y_pred2)
+accuracy3 = metrics.accuracy_score(y_test, y_pred3)
+
+print(f"[NAIVE BAYES] Accuracy: {accuracy}")
+print(f"[RANDOM FOREST] Accuracy: {accuracy2}")
+print(f"[REGRESSÃO] Accuracy: {accuracy3}")
+
+# print(data)
 # print_dataset(data)
 # print_cloudword(data)
